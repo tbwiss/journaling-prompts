1ce6ab5c7e4d5215129af63ca3a7013ef9a7cac4
Thomas Wiss <thomas.wiss@neotechnology.com>
2023-11-06 12:15:46 +0100

    feat: more prompts

--- src/data/database.ts
+++ src/data/database.ts
@@ -1,5 +1,5 @@
-// Self-reflection (https://blog.gratefulness.me/self-reflection-journaling-ideas-for-you/)
 export const selfReflectionPrompts = [
+  // Source: Self-reflection (https://blog.gratefulness.me/self-reflection-journaling-ideas-for-you/)
   "What makes you feel calm?",
   "What makes you feel in control?",
   "What makes you feel powerful?",
@@ -75,8 +75,8 @@ export const selfReflectionPrompts = [
   "What are you most grateful for?",
 ];
 
-// https://dayoneapp.com/blog/journal-prompts/
 const evening = [
+  // Source: https://dayoneapp.com/blog/journal-prompts/
   "What are three things that went well today, and why?",
   "What were the highlights of my day?",
   "What are three things that I could have done differently today, and how can I learn from these experiences?",
@@ -110,204 +110,167 @@ const evening = [
   "Who made a positive impact on my day and how?",
   "What did I do to make someone else's day better?",
   "What are some things I want to remember about today?",
+
+  // Source: ChatGPT
+  "Reflect on three positive moments from your day and why they made you happy.",
+  "What challenges did you face today, and how did you overcome them?",
+  "Describe a moment when you felt a sense of accomplishment or pride today.",
+  "Write about an act of kindness you witnessed or participated in.",
+  "What are three things you're grateful for today?",
+  "Explore your emotions: How are you feeling right now, and why?",
+  "Write about a book, movie, or TV show you enjoyed and why it resonated with you.",
+  "Set an intention for tomorrow: What do you hope to achieve or experience?",
+  "What did you learn today, and how can you apply this knowledge in the future?",
+  "Describe your ideal evening routine for winding down and relaxing.",
+  "Write about a personal goal you're currently working on and your progress.",
+  "Share any thoughts or ideas that have been on your mind lately.",
+  "Who is someone you're grateful to have in your life, and why?",
+  "Reflect on your personal strengths and how you used them today.",
+  "Write a letter to your future self, sharing your hopes and aspirations.",
+  "What's something that brought you joy today, no matter how small?",
+  "Describe a place you'd like to visit or a travel experience you dream of having.",
+  "How did you practice self-care or self-compassion today?",
+  "Explore a hobby or interest: What's something new you learned or discovered about it today?",
+  "Share a memory from the past that you cherish and explain why it's meaningful.",
 ];
 
 const stressRelief = [
+  // Source: https://dayoneapp.com/blog/journal-prompts/
   "What are some of the sources of stress in my life right now?",
-
   "How have I been coping with stress in the past?",
-
   "What are some healthy coping mechanisms I can use to manage stress?",
-
   "How can I prioritize self-care to reduce stress?",
-
   "What are some positive affirmations I can tell myself to combat stress?",
-
   "Who can I reach out to for support and encouragement when I am feeling stressed?",
-
   "How can I reframe negative thoughts and maintain a positive outlook?",
-
   "What are some activities or hobbies that help me relax and de-stress?",
-
   "How can I create a stress-free environment at home or at work?",
-
   "What are some steps I can take to prevent stress from overwhelming me in the future?",
-
   "What are some practical solutions to the sources of stress in my life?",
-
   "How can I prioritize my time and responsibilities to reduce stress?",
-
   "What are some physical activities I can do to relieve stress?",
-
   "How can I maintain a healthy work-life balance to reduce stress?",
-
   "How can I stay organized and on track to reduce stress?",
-
   "How can I find humor and joy in life to combat stress?",
-
   "What are some self-reflection exercises I can do to reduce stress?",
-
   "How can I maintain a healthy lifestyle to reduce stress, such as eating well, getting enough sleep, and exercising regularly?",
-
   "How can I set realistic expectations and boundaries to reduce stress?",
-
   "What are some things I can do to maintain a positive and relaxed state of mind, such as meditating, practicing mindfulness, or spending time in nature?",
 ];
 
 const selfRef = [
+  // Source: https://dayoneapp.com/blog/journal-prompts/
   "What are some values that are important to me, and how do they guide my decisions and actions?",
-
   "What are some experiences from my past that have shaped who I am today, and how have they influenced my beliefs and attitudes?",
-
   "What are some things that bring me joy and fulfillment, and how can I incorporate more of them into my life?",
-
   "What are some patterns of behavior or thought that hold me back, and how can I work to break those patterns?",
-
   "What are some goals or aspirations I have for my life, and what steps can I take to work towards them?",
-
   "What are some fears or insecurities that hold me back, and how can I work to overcome them?",
-
   "What are some relationships that are important to me, and how can I nurture and strengthen those relationships?",
-
   "What are some mistakes or failures from my past that have taught me valuable lessons, and how can I apply those lessons to my current life?",
-
   "What are some self-care practices that are important to me, and how can I make them a regular part of my routine?",
-
   "What are some things that I am grateful for in my life, and how can I cultivate more gratitude on a daily basis?",
 ];
 
 const relationship = [
+  // Source: https://dayoneapp.com/blog/journal-prompts/
   "How do I communicate my needs and boundaries in my relationships?",
-
   "What are some ways I can deepen my connections with loved ones?",
-
   "What are my values and priorities when it comes to relationships?",
-
   "How do these values influence my actions and choices?",
-
   "How do I respond to conflict in my relationships?",
-
   "What communication patterns do I notice when things get difficult?",
-
   "What are some ways I can show appreciation and gratitude for the people in my life?",
-
   "How do I express love and affection?",
-
   "What are some challenges I face in my relationships? How can I work on improving these challenges?",
-
   "How do I handle disagreements or differences in opinion with my loved ones? What are some healthy ways to approach these situations?",
-
   "What are my relationship goals?",
-
   "What do I want to achieve in my current relationships or in future relationships?",
-
   "What are some areas where I need to work on boundaries in my relationships? How can I create healthier boundaries?",
-
   "How do I balance my needs with the needs of my partner or loved ones? What are some ways to ensure both parties feel heard and respected?",
-
   "How do I manage stress and emotions in my relationships?",
-
   "What are some techniques for managing anxiety or other difficult emotions in relationships?",
-
   "What is my love language? How do I communicate love and affection to my partner or loved ones?",
 ];
 
 const mindfulness = [
+  // Source: https://dayoneapp.com/blog/journal-prompts/
   "What is happening in this present moment?",
-
   "What are five things I can see right now, and what colors, shapes, and textures do they have?",
-
   "If my mind was like the ocean right now, what is the water like?",
-
   "What thoughts am I observing right now?",
-
   "What sensory information am I getting in this present moment?",
-
   "What are three things I can hear right now, and how do they sound?",
-
   "What are three things I can feel physically right now, such as the weight of my body on a chair or the texture of my clothing?",
-
   "What are three things I can smell right now, and how do they smell?",
-
   "What are three things I can taste right now, and how do they taste?",
-
   "What emotions am I feeling right now, and how can I practice acceptance and self-compassion towards them?",
-
   "What thoughts are running through my mind right now, and how can I acknowledge them without getting caught up in them?",
-
   "What are three things I am looking forward to in the next hour, and how can I stay present and open to experiencing them fully?",
-
   "What are three things that are worrying me right now, and how can I practice mindfulness to reduce my stress and anxiety?",
-
   "What are three small actions I can take right now to bring myself into the present moment, such as taking a deep breath, stretching, or savoring a sip of tea or coffee?",
 ];
 
 const growthMindset = [
+  // Source: https://dayoneapp.com/blog/journal-prompts/
   "What are some areas of my life where I tend to have a fixed mindset?",
-
   "How can I shift my thinking to adopt a growth mindset instead?",
-
   "What are some goals that I've been afraid to pursue due to fear of failure or rejection?",
-
   "How can I reframe my mindset to view failure as a natural part of the learning process, and use it as an opportunity for growth?",
-
   "What are some of my limiting beliefs and self-talk that may be holding me back?",
-
   "How can I challenge and overcome them?",
-
   "How can I embrace challenges and failures as opportunities for growth and development, rather than viewing them as setbacks?",
-
   "How can I cultivate a positive and optimistic attitude, even in the face of adversity and difficulty?",
-
   "What are some ways I can seek out feedback and constructive criticism to continue growing and improving?",
-
   "How can I strive for progress, rather than perfection, in my personal and professional life?",
-
   "What are some of my strengths and areas for growth, and how can I use this knowledge to drive personal development and growth?",
-
   "How can I seek out new experiences, opportunities, and relationships to broaden my horizons and support personal growth?",
-
   "How can I foster resilience and perseverance in the face of obstacles and challenges to continue growing and developing?",
-
   "How can I take responsibility for my thoughts, feelings, and actions, and use them as opportunities for growth and development?",
-
   "How can I view mistakes and failures as learning opportunities, rather than setbacks or obstacles?",
-
   "What are some new skills or knowledge areas that I want to develop?",
-
   "How can I cultivate a curious and open-minded attitude, and seek out new information and knowledge to support growth and development?",
-
   "What are some ways I can adopt a proactive, rather than reactive, approach to challenges and difficulties?",
 ];
 
 const gratitude = [
+  // Source: https://dayoneapp.com/blog/journal-prompts/
   "What are three things I am grateful for today?",
-
   "Who are three people in my life that I am grateful for, and why?",
-
   "What are three small things that happened today that I am grateful for?",
-
   "What is one thing that I often take for granted in my life, and how can I cultivate more appreciation and gratitude for it?",
-
   "What are some positive qualities or strengths that I possess, and how can I be grateful for them?",
-
   "What is something in my life that I feel 'lucky' to have?",
-
   "What is a simple delight I have been enjoying lately?",
-
   "What is something I am grateful to have learned recently?",
-
   "In what ways have I grown as a person over the last year?",
-
   "What do I like about where I live right now?",
-
   "What were some moments of joy today?",
-
   "How does expressing gratitude make me feel right now?",
-
   "How can I show my gratitude today?",
-
   "What are some ways I can express gratitude and appreciate the beauty and wonder of the world around me?",
+
+  // Source: ChatGPT
+  "What are three things you're grateful for in your life right now?",
+  "Write about a person who has had a positive influence on your life and why you're grateful for their presence.",
+  "Reflect on a recent challenge or obstacle you overcame and express gratitude for the lessons learned.",
+  "Describe a small, everyday pleasure that you often take for granted but are grateful for.",
+  "Write about a place that holds special meaning for you and the reasons it makes you feel grateful.",
+  "Share a moment when a stranger showed you kindness or made your day better.",
+  "Reflect on a skill or talent you possess and why you're grateful for it.",
+  "Express gratitude for the support and love you receive from your friends and family.",
+  "Write about a teacher or mentor who made a positive impact on your life and what you're thankful for in their guidance.",
+  "Share a recent accomplishment or milestone you're proud of and express your gratitude for the progress you've made.",
+  "Reflect on the beauty of nature and describe a natural wonder or scenery you're grateful to have experienced.",
+  "Write about a book, quote, or piece of art that has inspired you and why it brings you gratitude.",
+  "Express gratitude for your physical health and well-being. What do you appreciate about your body?",
+  "Share a memory from your past that you're grateful for and how it continues to bring you joy.",
+  "Write about a moment when you received unexpected help or support, and why you're thankful for it.",
+  "Reflect on the freedoms and opportunities you have in your life and express gratitude for them.",
+  "Describe a recent act of kindness you performed for someone else and how it made you feel grateful.",
+  "Share something specific you love about the changing seasons and why it brings you gratitude.",
+  "Express gratitude for the technology or tools that make your daily life more convenient or enjoyable.",
+  "Write about a personal quality or trait you admire in yourself and why you're grateful for it.",
 ];
 
 export type Category = {
